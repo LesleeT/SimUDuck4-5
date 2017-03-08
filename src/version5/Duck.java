b724b4ed66fcce3dc3e56552e180f8790d7978b4
Leslee <Leslee@DESKTOP-273V837>
2017-03-08 12:38:00 -0600

    initial commit

---
+++ src/version5/Duck.java
@@ -0,0 +1,38 @@
+package version5;
+
+/**
+ * The main difference with this 5th version is that the class
+ * provides methods for setting fly and quack behaviors at runtime.
+ * Otherwise, everything else is the same.
+ * 
+ * @author jlombardo
+ *
+ */
+public interface Duck {
+	// Instance variables of interface type
+	// Each duck has a reference to something that implements the behaviors
+	// via an interface. Hey, isn't this composition the DIP way!!!
+	// Only problem is we still have the limitation that all Ducks
+	// have fly and quack behaviors, even thought that is not always needed.
+	//private FlyBehavior flyBehavior;
+	//private QuackBehavior quackBehavior;
+	
+	
+	public abstract void display();
+	
+	/*
+	 * Rather than handling the quack behavior itself, the Duck object
+	 * delegates that behavior to the object referenced by quackBehavior.
+	 * This object is unknown -- that's DIP in action!
+	 */
+	public abstract void performQuack();
+	
+	public abstract void performFly();
+	
+        public abstract void setFlyBehavior(FlyBehavior flyBeh);
+        
+        public abstract void setQuackBehavior(QuackBehavior quackBeh);
+	
+	
+	// OTHER DUCK-LIKE METHODS
+}
---
+++ src/version5/ModelDuck.java
@@ -0,0 +1,34 @@
+package version5;
+
+public class ModelDuck implements Duck {
+    private FlyBehavior flyBehavior;
+    private QuackBehavior quackBehavior;
+
+    public ModelDuck(FlyBehavior flyBehavior, QuackBehavior quackBehavior) {
+        this.flyBehavior = flyBehavior;
+        this.quackBehavior = quackBehavior;
+    }
+
+    public final FlyBehavior getFlyBehavior() {
+        return flyBehavior;
+    }
+
+    public final void setFlyBehavior(FlyBehavior flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+
+    public QuackBehavior getQuackBehavior() {
+        return quackBehavior;
+    }
+
+    public void setQuackBehavior(QuackBehavior quackBehavior) {
+        this.quackBehavior = quackBehavior;
+    }
+
+    
+
+	
+	public final void display() {
+		System.out.println("I'm a model duck");
+	}
+}
